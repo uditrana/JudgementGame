3539cd62807596840eae0b2bfd04725f653b367e
uditrana <uditrana@outlook.com>
2020-08-01 14:21:25 -0700

    updates to scoresheet

--- scoresheet.py
+++ scoresheet.py
@@ -1,2 +1,19 @@
 class ScoreSheet(object):
+    def __init__(self):
+        self.sheet = dict() # pid to list of scores
+        self.currBids = dict() # pid to list of bids
+
+    def bidToScore(self, bid):
+        return bid*11+10
     
+    def sumOfBids(self):
+        return sum(self.currBids)
+
+    def getBid(self, pid):
+        return self.currBids[pid]
+    
+    def getScore(self, pid):
+        return sum(self.sheet[pid])
+
+    def addBid(self, pid, bid):
+        self.currBids[pid] = bid

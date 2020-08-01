f384109ef82d40fbf67e0d039eaa5a486ffd01ae
uditrana <uditrana@outlook.com>
2020-08-01 16:03:15 -0700

    merged

--- player.py
+++ player.py
@@ -3,6 +3,14 @@ class Player(object):
         self.name = name
         self.id = id
         self.hand = []
+        self.tricks = []
+
 
     def getHandString(self):
         return ', '.join([card.getString() for card in self.hand])
+<<<<<<< Updated upstream
+=======
+
+    def getTricksWon(self):
+        return len(self.tricks)
+>>>>>>> Stashed changes

97dbee7793419868256c25628b91fc1a4042b397
uditrana <uditrana@outlook.com>
2020-08-01 17:41:25 -0700

    minor updates

--- player.py
+++ player.py
@@ -1,10 +1,11 @@
 class Player(object):
-    def __init__(self, name, id):
+    def __init__(self, id, name=None):
+        if name == None:
+            name = "Player " + str(id)
         self.name = name
         self.id = id
         self.hand = []
         self.tricks = []
-
 
     def getHandString(self):
         return ', '.join([card.getString() for card in self.hand])

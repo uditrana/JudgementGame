76aa02dbe1827ca5074bc546dd637d259f783efd
uditrana <uditrana@outlook.com>
2020-08-01 17:00:47 -0700

    scoresheet api additions

--- scoresheet.py
+++ scoresheet.py
@@ -1,19 +1,29 @@
+def bidToScore(bid):
+    return bid*11+10
+
 class ScoreSheet(object):
     def __init__(self):
         self.sheet = dict() # pid to list of scores
         self.currBids = dict() # pid to list of bids
-
-    def bidToScore(self, bid):
-        return bid*11+10
     
-    def sumOfBids(self):
-        return sum(self.currBids)
+    def getSumOfBids(self):
+        return sum(list(self.currBids.values()))
+    
+    def getTotalScore(self, pid):
+        return sum(self.sheet[pid])
+    
+    def getRoundScore(self, pid, round): # round is zero indexed
+        return self.sheet[pid][round]
 
+    def setBid(self, pid, bid):
+        self.currBids[pid] = bid
+    
     def getBid(self, pid):
         return self.currBids[pid]
-    
-    def getScore(self, pid):
-        return sum(self.sheet[pid])
 
-    def addBid(self, pid, bid):
-        self.currBids[pid] = bid
+    def convertPlayerBidToScore(self, pid, didWin):
+        if didWin:
+            self.addScore(pid, self.bidToScore(self.getBid(pid)))
+        else:
+            self.addScore(pid, 0)
+        self.currBids.pop(pid)
